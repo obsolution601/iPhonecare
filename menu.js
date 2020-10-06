c5bae6b52606ed03a7fde84cece09ae7be72dfd2
obsolution601 <69605414+obsolution601@users.noreply.github.com>
2020-10-06 04:24:13 -0300

    yes updated

---
+++ menu.js
@@ -0,0 +1,27 @@
+< img src = "download.jpg"
+width = "30%"
+height = "30%" / >
+    <
+    img src = "iphone 11pro.jpg"
+width = "30%"
+height = "30%"
+class = "iphone_pro" / >
+
+
+
+
+    <
+    /div> <
+    /a> <
+    div class = "photo_upload" >
+    <
+    a href = "#"
+id = "IPHONE 7" > < img src = "iphone 7 red.jpg"
+width = "30%"
+height = "30%" / > < /a> <
+    h2 > < a href = "#"
+class = "iphone7_red" > IPHONE 7 RED < /a></h
+2 >
+
+    <
+    /div>
